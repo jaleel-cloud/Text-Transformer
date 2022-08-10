85fe93de76aebd0ac7c5fc2f30c1c7803166784c
jaleel-cloud <68658592+jaleel-cloud@users.noreply.github.com>
2022-08-10 12:54:47 +0530

    Updates

---
+++ precache-manifest.24282526b9c0195cdc10230245a26404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5de2814d1e039ac040f",
+    "url": "/Text-Transformer/static/css/main.acf8c675.chunk.css"
+  },
+  {
+    "revision": "b5de2814d1e039ac040f",
+    "url": "/Text-Transformer/static/js/main.b5de2814.chunk.js"
+  },
+  {
+    "revision": "24758a8b680d33a928be",
+    "url": "/Text-Transformer/static/js/1.24758a8b.chunk.js"
+  },
+  {
+    "revision": "20d065d376f06b32d611",
+    "url": "/Text-Transformer/static/js/runtime~main.20d065d3.js"
+  },
+  {
+    "revision": "cc4378f0f6278ae333ea346c7c522bf6",
+    "url": "/Text-Transformer/index.html"
+  }
+];
